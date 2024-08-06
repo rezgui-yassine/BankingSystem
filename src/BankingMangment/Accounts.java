b018e4225564a70c7ef07d701ebf245e013e5728
Rezgui Yassine <rezguiyassine224@gmail.com>
2024-08-06 13:33:10 +0100

    first commit

---
+++ src/BankingMangment/Accounts.java
@@ -0,0 +1,10 @@
+package BankingMangment;
+
+public class Accounts {
+    private String accountNumber;
+    private double balance;
+    public Accounts(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+}
